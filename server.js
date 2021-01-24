505f94f2b503e6967dd73ae55f59e0abb887f6fe
simpingtech <justinej.olivar@gmail.com>
2021-01-25 02:27:48 +0800

    commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 const parser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const app = express();
 var role = null;
 
@@ -22,11 +23,23 @@ const pool = new Pool({
 
 var portaccess = process.env.PORT || 8080;
 
+/*
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
+*/
+
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000
+  }),
+  resave: false,
+  secret: 'secret',
+	saveUninitialized: true
+}))
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
