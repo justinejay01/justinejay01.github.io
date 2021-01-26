259582d90182314d4d392d8c0498d715c754893f
simpingtech <justinej.olivar@gmail.com>
2021-01-26 16:17:33 +0800

    c

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 //const axios = require('axios');
 //const HttpsProxyAgent = require('https-proxy-agent');
 //const jwt = require('jsonwebtoken');
-const { Pool, Client } = require('pg');
-const path = require('path');
+const { Pool, Client } = require("pg");
+const path = require("path");
 //const mysql = require('mysql');
-const parser = require('body-parser');
-const express = require('express');
-const session = require('express-session');
-const MemoryStore = require('memorystore')(session);
+const parser = require("body-parser");
+const express = require("express");
+const session = require("express-session");
+const MemoryStore = require("memorystore")(session);
 const app = express();
 var role = null;
 
@@ -17,8 +17,8 @@ var role = null;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 var portaccess = process.env.PORT || 8080;
@@ -34,145 +34,161 @@ app.use(session({
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(session({
-  cookie: { maxAge: 86400000 },
-  store: new MemoryStore({
-    checkPeriod: 86400000
-  }),
-  resave: false,
-  secret: 'secret',
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000,
+    }),
+    resave: false,
+    secret: "secret",
+    saveUninitialized: true,
+  })
+);
 
-app.get('/', function (req, res) {
+const parseJsonAsync = (jsonString) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(JSON.parse(jsonString));
+    });
+  });
+};
+
+app.get("/", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/index.html', { root: __dirname });
+    res.sendFile("/index.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/programs', function (req, res) {
+app.get("/programs", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/programs/index.html', { root: __dirname });
+    res.sendFile("/programs/index.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/programs/csharp', function (req, res) {
+app.get("/programs/csharp", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/programs/csharp/index.html', { root: __dirname });
+    res.sendFile("/programs/csharp/index.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/programs/csharp/lesson-6', function (req, res) {
+app.get("/programs/csharp/lesson-6", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/programs/csharp/lesson-6.html', { root: __dirname });
+    res.sendFile("/programs/csharp/lesson-6.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/programs/csharp/lesson-7', function (req, res) {
+app.get("/programs/csharp/lesson-7", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/programs/csharp/lesson-7.html', { root: __dirname });
+    res.sendFile("/programs/csharp/lesson-7.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/tools', function (req, res) {
+app.get("/tools", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/tools/index.html', { root: __dirname });
+    res.sendFile("/tools/index.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/tools/gdrive-linkgen', function (req, res) {
+app.get("/tools/gdrive-linkgen", function (req, res) {
   if (req.session.loggedin) {
-    res.sendFile('/tools/gdrive-linkgen.html', { root: __dirname });
+    res.sendFile("/tools/gdrive-linkgen.html", { root: __dirname });
   } else {
-    res.redirect('/auth/');
+    res.redirect("/auth/");
   }
 });
 
-app.get('/auth/', function (req, res) {
+app.get("/auth/", function (req, res) {
   if (req.session.loggedin) {
-    res.redirect('/');
+    res.redirect("/");
   } else {
-    res.sendFile('/auth.html', { root: __dirname });
+    res.sendFile("/auth.html", { root: __dirname });
   }
 });
 
-app.post('/auth/login', function (req, res) {
+app.post("/auth/login", function (req, res) {
   var uname = req.body.uname;
   var pword = req.body.pword;
 
   if (uname && pword) {
     pool
-      .query('SELECT role FROM users.auth where uname = $1 and pword = $2', [uname, pword])
-      .then(resu => {
+      .query("SELECT role FROM users.auth where uname = $1 and pword = $2", [
+        uname,
+        pword,
+      ])
+      .then((resu) => {
         if (resu.rows[0] != null) {
           //var auth = JSON.parse(resu.rows[0]);
           //console.log(auth.role);
           //role = auth.role;
           req.session.loggedin = true;
           req.session.username = uname;
-          res.send('1');
+          res.send("1");
         } else {
-          res.send('0');
+          res.send("0");
         }
       })
-      .catch(err =>
+      .catch((err) =>
         setImmediate(() => {
-          throw err
+          throw err;
         })
-      )
+      );
   } else {
-    res.send('Please enter username and/or password!');
+    res.send("Please enter username and/or password!");
     res.end();
   }
 });
 
-app.post('/auth/reg', function (req, res) {
+app.post("/auth/reg", function (req, res) {
   var uname = req.body.uname;
   var email = req.body.email;
   var pword = req.body.pword;
 
   if (uname && pword && email) {
     pool
-      .query('SELECT count(uname) FROM users.auth where uname = $1', [uname])
-      .then(resu => {
-        //res.send(resu.rows[0]);
-        var cnt = JSON.parse(resu.rows[0]);
-        if (cnt.count != 0) {
-          res.send('2');
+      .query("SELECT count(uname) FROM users.auth where uname = $1", [uname])
+      .then((resu) => {
+        parseJsonAsync(data)
+          .then((jsonData) => console.log(jsonData));
+        if (resu.rows[0] != 0) {
+          res.send("2");
         } else {
           pool
-            .query('INSERT INTO users.auth (id,uname,pword,email,role) values (0,$1,$2,$3,$4)', [uname,pword,email,'user'])
-            .then(resu => {
+            .query(
+              "INSERT INTO users.auth (id,uname,pword,email,role) values (0,$1,$2,$3,$4)",
+              [uname, pword, email, "user"]
+            )
+            .then((resu) => {
               res.send(resu.rows[0]);
             })
-            .catch(err =>
+            .catch((err) =>
               setImmediate(() => {
                 console.log(err);
-                throw err
+                throw err;
               })
-            )
+            );
         }
       })
-      .catch(err =>
+      .catch((err) =>
         setImmediate(() => {
           console.log(err);
-          throw err
+          throw err;
         })
-      )
+      );
   } else {
-    res.send('Please enter username and/or password!');
+    res.send("Please enter username and/or password!");
     res.end();
   }
 });
