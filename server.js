bfb466195b51298a0e4fad31ae480ebdf554f2d8
simpingtech <justinej.olivar@gmail.com>
2021-01-26 10:38:11 +0800

    c

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.post('/auth/reg', function (req, res) {
   var email = req.body.email;
   var pword = req.body.pword;
 
-  if (uname > 0 && pword > 0 && email > 0) {
+  if (uname && pword && email) {
     pool
       .query('SELECT count(uname) FROM users.auth where uname = $1', [uname])
       .then(resu => {
